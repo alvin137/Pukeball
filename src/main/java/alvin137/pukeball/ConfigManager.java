a5264f35deaa8778831d181303f11669baaa9f66
alvin137 <alvin1377@gmail.com>
2017-01-30 12:26:19 +0900

    동료부르기 메소드
    -camera를 돌려야 함

--- src/main/java/alvin137/pukeball/ConfigManager.java
+++ src/main/java/alvin137/pukeball/ConfigManager.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraftforge.common.config.Configuration;
 
 public class ConfigManager {
@@ -13,7 +14,7 @@ public class ConfigManager {
 	public static final String CATEGORY_MOBS = "Mobs";
 	
 	public static Map<String, Integer> map = new HashMap<String, Integer>();
-	
+	//public static double CatchRate;
 	public static void setConfig(Configuration config) {
 		List<String> list = EntityList.getEntityNameList();
 		config.load();
@@ -21,10 +22,12 @@ public class ConfigManager {
 		//map.put(list.get(i), config.getFloat(list.get(i), CATEGORY_MOBS, 30, 0, 255, list.get(i)+"Catch rate"));
 		map.put(list.get(i), config.get(CATEGORY_MOBS, list.get(i), 30).getInt());
 		}
+		//CatchRate = config.get(CATEGORY_MOBS, "Catchrate", 50, "All catch rate, divided by each entity's max health", 0, 255).getDouble();
 		config.save();
 	}
 	
-	public static int getConfig(String name) {
+	public static double getConfig(String name) {
 		return map.get(name);
+		//return (CatchRate / e.getMaxHealth());
 	}
 }
--- src/main/java/alvin137/pukeball/entity/EntityPukeBall.java
+++ src/main/java/alvin137/pukeball/entity/EntityPukeBall.java
@@ -13,16 +13,26 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IEntityLivingData;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttribute;
+import net.minecraft.entity.ai.attributes.RangedAttribute;
 import net.minecraft.entity.monster.EntityBlaze;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumHandSide;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.datafix.DataFixer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 
 public class EntityPukeBall extends EntityThrowable {
@@ -59,29 +69,28 @@ public class EntityPukeBall extends EntityThrowable {
 		if (this.worldObj.isRemote)
 			return;
 		else if (this.getEntityData().hasKey(NBTKeys.KEY_ENTITY)) {
-			if (!this.worldObj.isRemote) {
-				Entity e = EntityList.createEntityFromNBT(this.getEntityData().getCompoundTag(NBTKeys.KEY_ENTITY),
-						this.worldObj);
-				e.resetEntityId();
-				e.setPosition(posX, posY, posZ);
-				e.setUniqueId(UUID.randomUUID());
-				this.worldObj.spawnEntityInWorld(e);
-				PukeBall.logger.info(this.getEntityData().getCompoundTag(NBTKeys.KEY_ENTITY).getString("id"));
-			}
+			Entity e = EntityList.createEntityFromNBT(this.getEntityData().getCompoundTag(NBTKeys.KEY_ENTITY),
+					this.worldObj);
+			e.resetEntityId();
+			e.setPosition(posX, posY, posZ);
+			e.setUniqueId(UUID.randomUUID());
+			this.worldObj.spawnEntityInWorld(e);
+			PukeBall.logger.info(this.getEntityData().getCompoundTag(NBTKeys.KEY_ENTITY).getString("id"));
 		} else if (result.entityHit != null && result.entityHit.isEntityAlive() && classVerify(result.entityHit)) {
-			EntityLivingBase e = (EntityLivingBase)result.entityHit;
-			if(getCatchrate(e)){
-			ItemStack ball = new ItemStack(CommonProxy.ball);
-			ball.setTagCompound(new NBTTagCompound());
-			ball.getTagCompound().setTag(NBTKeys.KEY_ENTITY, e.serializeNBT());
-			ball.getTagCompound().setString(NBTKeys.KEY_ENTITY_NAME, e.getName());
-			result.entityHit.entityDropItem(ball, 1);
-			result.entityHit.setDead();
-			}
+			EntityLivingBase e = (EntityLivingBase) result.entityHit;
+			if (isCatchSuccess(e)) {
+				ItemStack ball = new ItemStack(CommonProxy.ball);
+				ball.setTagCompound(new NBTTagCompound());
+				ball.getTagCompound().setTag(NBTKeys.KEY_ENTITY, e.serializeNBT());
+				ball.getTagCompound().setString(NBTKeys.KEY_ENTITY_NAME, e.getName());
+				result.entityHit.entityDropItem(ball, 1);
+				result.entityHit.setDead();
+				return;
+			} 
+			callFriends(e);
 		}
 		// 이 구문은 한 블럭 내려가야 모든 상황에서 실행될 수 있겠죠. 안내려가면 몹한테 안 맞는 이상 안죽음.
-		if (!this.worldObj.isRemote)
-			this.setDead();
+		this.setDead();
 	}
 
 	/**
@@ -92,23 +101,68 @@ public class EntityPukeBall extends EntityThrowable {
 		return entity instanceof EntityLiving && entity.isNonBoss();
 	}
 
-	public boolean getCatchrate(EntityLivingBase e) {
-		double ballBonus = 1.0F; // 추후수정
-		double statusBonus = 1.0F; // 추후수정
-		double a = (((3 * e.getMaxHealth() - 2 * e.getHealth()) * ConfigManager.getConfig(e.getName()) * ballBonus) / 3*e.getMaxHealth()) * statusBonus;
-		PukeBall.logger.info("calculated" + a);
-		Random r = new Random();
+	public boolean isCatchSuccess(EntityLivingBase e) {
+		double ballBonus = 1, statusBonus = 1, mhp = e.getMaxHealth(), hp = e.getHealth(),
+				// a = ((3 * mhp - 2 * hp) *
+				// ConfigManager.getConfig(EntityList.getEntityString(e)) *
+				// ballBonus)
+				a = ((3 * mhp - 2 * hp) * ConfigManager.getConfig(EntityList.getEntityString(e))) / (3 * mhp)
+						* statusBonus,
+				b = 65536 / Math.pow((255 / a), 0.1875);
 		for (int i = 0; i < 4; i++) {
-			if (65536 / Math.pow((255 / a), 0.1875) < r.nextInt(65536)) {	
-				PukeBall.logger.info("ASDFASDF");
+			if (b < e.getRNG().nextInt(65536)) {
+				PukeBall.logger.info(String.format(
+						"Mob %s catch failed in %dth shake. ballBonus = %f, statusBonus = %f,  mhp = %f, hp = %f, a = %f, b = %f, Catch Rate = %f",
+						EntityList.getEntityString(e), i + 1, ballBonus, statusBonus, mhp, hp, a, b,
+						ConfigManager.getConfig(EntityList.getEntityString(e))));
 				return false;
 			}
-			else{
-				PukeBall.logger.info("moving");
-			}
 		}
-		PukeBall.logger.info("Catching" + e.getName());
+		PukeBall.logger.info(String.format(
+				"Mob %s catch Successed. ballBonus = %f, statusBonus = %f,  mhp = %f, hp = %f, a = %f, b = %f, Catch Rate = %f",
+				EntityList.getEntityString(e), ballBonus, statusBonus, mhp, hp, a, b,
+				ConfigManager.getConfig(EntityList.getEntityString(e))));
 		return true;
 
+	}
+
+	public void callFriends(EntityLivingBase e) {
+		this.getThrower().setRotationYawHead(180f);
+			EntityLivingBase entitylivingbase = this.getThrower();
+			int i = MathHelper.floor_double(e.posX);
+			int j = MathHelper.floor_double(e.posY);
+			int k = MathHelper.floor_double(e.posZ); 
+				EntityLivingBase entity = (EntityLivingBase) EntityList.createEntityByName(EntityList.getEntityString(e), this.worldObj);
+
+				for (int l = 0; l < 50; ++l) {
+					int i1 = i + MathHelper.getRandomIntegerInRange(this.rand, 1, 10)
+							* MathHelper.getRandomIntegerInRange(this.rand, -1, 1);
+					int j1 = j + MathHelper.getRandomIntegerInRange(this.rand, 1, 10)
+							* MathHelper.getRandomIntegerInRange(this.rand, -1, 1);
+					int k1 = k + MathHelper.getRandomIntegerInRange(this.rand, 1, 10)
+							* MathHelper.getRandomIntegerInRange(this.rand, -1, 1);
+
+					if (this.worldObj.getBlockState(new BlockPos(i1, j1 - 1, k1)).isSideSolid(this.worldObj,
+							new BlockPos(i1, j1 - 1, k1), net.minecraft.util.EnumFacing.UP)) {
+						entity.setPosition((double) i1, (double) j1, (double) k1);
+
+						if (!this.worldObj.isAnyPlayerWithinRangeAt((double) i1, (double) j1, (double) k1, 7.0D)
+								&& this.worldObj.checkNoEntityCollision(entity.getEntityBoundingBox(),
+										entity)
+								&& this.worldObj.getCollisionBoxes(entity, entity.getEntityBoundingBox())
+										.isEmpty()
+								&& !this.worldObj.containsAnyLiquid(entity.getEntityBoundingBox())) {
+							e.worldObj.spawnEntityInWorld(entity);
+							PukeBall.logger.info("entity successfuly spawned");
+							if (entitylivingbase != null)
+								((EntityLiving) entity).setAttackTarget(entitylivingbase);
+							((EntityLiving) entity).onInitialSpawn(
+									this.worldObj.getDifficultyForLocation(new BlockPos(entity)),
+									(IEntityLivingData) null);
+							break;
+						}
+					}
+				}
+				
 	}
 }
